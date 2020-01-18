e4a4e02ab130e110d3ccf2d07996dbd3965b30f4
Hamza ali <sa02912@st.habib.edu.pk>
2020-01-18 15:13:43 +0500

    Ready to make

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'header', component: HeaderComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
